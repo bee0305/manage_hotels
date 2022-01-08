59267a7dff380294731af8222acb3db19cf2b3e5
bee0305 <rietveldtt@gmail.com>
2022-01-08 19:23:43 +0100

    change tests for api poins via menu(admin perms): only request head

--- city/tests/test_csv_import.py
+++ city/tests/test_csv_import.py
@@ -1,16 +1,26 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
 from django.test import TestCase, Client
-from django.urls import reverse
+from django.conf import settings
+
 
 
 class TestViews(TestCase):
 
     def setUp(self):
         self.client = Client()
-        self.api_url = reverse('city:load-cities')
+        self.api_url_cities = settings.CITY_URL
+        self.api_url_hotels = settings.HOTEL_URL
 
-    def test_get_ajax(self):
-        """test api point"""
-        response = self.client.get(self.api_url)
-
+    def test_get_ajax_cities(self):
+        """test api point cities status Ok and template """
+        response = requests.head(url=self.api_url_cities, auth=HTTPBasicAuth(settings.CSV_HOST, settings.CSV_PSW))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'cities/show_cities.html')
+        
+
+    def test_get_ajax_hotels(self):
+        """test api point hotels status Ok and template"""
+        response = requests.head(url=self.api_url_hotels, auth=HTTPBasicAuth(settings.CSV_HOST, settings.CSV_PSW))        
+        self.assertEqual(response.status_code, 200)
+        
