2bb51440950e4c8d79c3ee68ee05bdff40548d01
bee0305 <rietveldtt@gmail.com>
2022-01-18 22:18:24 +0100

    add readme file

---
+++ README.md
@@ -0,0 +1,19 @@
+# Small project: fetch data from api point and save it either in db or csv file
+#### Authenticated users(hotel managers) can crud hotels in a given city
+
+#### Core: django 4.0, requests
+#### Extra's
+
+1. custom ajax
+
+2. htmx
+
+3. django-ajax-select
+
+4. custom mamangement commands
+
+5. management commands based on django-extentions
+
+6. cron job
+
+7. Unit tests + fixtures
--- city/admin.py
+++ city/admin.py
@@ -1,18 +1,54 @@
 from django.contrib import admin
+import django.apps
 from .models import City, Hotel
 from .forms import CityForm
+from users.models import HotelManager
 
+# models = django.apps.apps.get_models()
+# for model in models:
+#     try:
+#         admin.site.register(model)
+#     except admin.sites.AlreadyRegistered:
+#         print('already registered')
+# admin.site.unregister(django.contrib.sessions.models.Session)
+
+TIP ='Enter 3 chars to start search'
 
 @admin.register(Hotel)
 class HotelAdmin(admin.ModelAdmin):
     """"""
     form = CityForm
-    list_display = ('id','name','unid','city_code')
+    list_display = ('id','name','unid','city_code','get_city_name')
     list_filter = ( 'city',)
+    fieldsets = (
+        ('Section 1:Meta data',{
+            'fields':('city_code','unid'), 
+            'classes':('collapse',)
+        }),
+        ('Section 2: Hotel Name',{
+            'fields':('name',)
+        }),
+        ('Section 3: search for city',{
+            'fields':('city',),
+            'description':f'{TIP}'
+        })
+        
+    )
+    def get_city_name(self,obj):
+        return obj.city.name
 
+    get_city_name.short_description = 'City Name'  
+    # can be sorted
+    get_city_name.admin_order_field = 'city'      
 
 @admin.register(City)
 class CityForm(admin.ModelAdmin):
-    list_display = ('id','name','city_code')
+    list_display = ('id','name','city_code','get_city_hotel_manager')
 
+    def get_city_hotel_manager(self,obj):
+        if obj.managers.first():
+            return obj.managers.first()
+        else:
+            return 'No manager yet'    
+    get_city_hotel_manager.short_description = 'City Manager'    
 
